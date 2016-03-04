21a9958472a3184b0335a392c63fbad64c006152
Jan Ouwens <jan.ouwens@ordina.nl>
2016-03-04 13:48:03 +0100

    Adds initial implementation of the plugin

--- build.sbt
+++ build.sbt
@@ -4,9 +4,9 @@ name := "sbt-findsecbugs"
 organization := "nl.codestar"
 version := "0.1"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.10.6"
 scalacOptions ++= Seq("-encoding", "UTF8", "-Xfatal-warnings",
   "-deprecation", "-feature", "-unchecked", "-Xlint",
-  "-Ywarn-dead-code", "-Ywarn-unused", "-Ywarn-adapted-args"
+  "-Ywarn-dead-code", "-Ywarn-adapted-args"
 )
 
---
+++ src/main/scala/nl/codestar/sbtfindsecbugs/FindSecBugs.scala
@@ -0,0 +1,37 @@
+package nl.codestar.sbtfindsecbugs
+
+import java.io.File
+
+import sbt._
+import Keys._
+
+class FindSecBugs extends AutoPlugin {
+  val findsecbugsConfig = sbt.config("findsecbugs")
+  ivyConfigurations += findsecbugsConfig
+  libraryDependencies ++= Seq(
+    "com.google.code.findbugs" % "findbugs" % "3.0.1" % "findsecbugs->default",
+    "com.google.code.findbugs" % "jsr305" % "3.0.1" % "findsecbugs->default",
+    "com.h3xstream.findsecbugs" % "findsecbugs-plugin" % "1.4.5" % "findsecbugs->default"
+  )
+
+  lazy val findSecBugs = taskKey[Unit]("Perform FindSecurityBugs check")
+  findSecBugs := {
+    def commandLineClasspath(classpathFiles: Seq[File]): String = PathFinder(classpathFiles).absString
+    lazy val output = crossTarget.value / "findsecbugs" / "report.html"
+    lazy val findbugsClasspath = Classpaths managedJars (findsecbugsConfig, classpathTypes.value, update.value)
+    lazy val classpath = commandLineClasspath(findbugsClasspath.files)
+    lazy val auxClasspath = commandLineClasspath((dependencyClasspath in Compile).value.files ++ (findbugsClasspath.files filter (_.getName startsWith "jsr305")))
+    lazy val pluginList = s"${Path.userHome.absolutePath}/.ivy2/cache/com.h3xstream.findsecbugs/findsecbugs-plugin/jars/findsecbugs-plugin-1.4.5.jar"
+    lazy val includeList = baseDirectory.value / "project" / "findSecBugs-include.xml"
+
+    IO.createDirectory(output.getParentFile)
+    println("Performing FindSecurityBugs check...")
+    Fork.java(
+      ForkOptions(javaHome = javaHome.value, outputStrategy = Some(new LoggedOutput(streams.value.log))),
+      List("-Xmx1024m", "-cp", classpath, "edu.umd.cs.findbugs.LaunchAppropriateUI",
+        "-textui", "-html:plain.xsl", "-output", output.getAbsolutePath, "-nested:true", "-auxclasspath", auxClasspath,
+        "-low", "-effort:max", "-pluginList", pluginList, "-include", includeList.getAbsolutePath,
+        (classDirectory in Compile).value.getAbsolutePath)
+    )
+  }
+}
