ef972b8c464f2e4440311f69092451d4244b324e
Jan Ouwens <jan.ouwens@ordina.nl>
2016-03-04 15:03:21 +0100

    Adds hard-coded include list

--- src/main/scala/nl/codestar/sbtfindsecbugs/FindSecBugs.scala
+++ src/main/scala/nl/codestar/sbtfindsecbugs/FindSecBugs.scala
@@ -22,16 +22,33 @@ class FindSecBugs extends AutoPlugin {
     lazy val classpath = commandLineClasspath(findbugsClasspath.files)
     lazy val auxClasspath = commandLineClasspath((dependencyClasspath in Compile).value.files ++ (findbugsClasspath.files filter (_.getName startsWith "jsr305")))
     lazy val pluginList = s"${Path.userHome.absolutePath}/.ivy2/cache/com.h3xstream.findsecbugs/findsecbugs-plugin/jars/findsecbugs-plugin-1.4.5.jar"
-    lazy val includeList = baseDirectory.value / "project" / "findSecBugs-include.xml"
 
     IO.createDirectory(output.getParentFile)
     println("Performing FindSecurityBugs check...")
-    Fork.java(
-      ForkOptions(javaHome = javaHome.value, outputStrategy = Some(new LoggedOutput(streams.value.log))),
-      List("-Xmx1024m", "-cp", classpath, "edu.umd.cs.findbugs.LaunchAppropriateUI",
-        "-textui", "-html:plain.xsl", "-output", output.getAbsolutePath, "-nested:true", "-auxclasspath", auxClasspath,
-        "-low", "-effort:max", "-pluginList", pluginList, "-include", includeList.getAbsolutePath,
-        (classDirectory in Compile).value.getAbsolutePath)
-    )
+    IO.withTemporaryDirectory { tempdir =>
+      val includeFile: sbt.File = createIncludesFile(tempdir)
+
+      Fork.java(
+        ForkOptions(javaHome = javaHome.value, outputStrategy = Some(new LoggedOutput(streams.value.log))),
+        List("-Xmx1024m", "-cp", classpath, "edu.umd.cs.findbugs.LaunchAppropriateUI",
+          "-textui", "-html:plain.xsl", "-output", output.getAbsolutePath, "-nested:true", "-auxclasspath", auxClasspath,
+          "-low", "-effort:max", "-pluginList", pluginList, "-include", includeFile.getAbsolutePath,
+          (classDirectory in Compile).value.getAbsolutePath)
+      )
+    }
+
+    def createIncludesFile(tempdir: sbt.File): sbt.File = {
+      val includeFile = tempdir / "include.xml"
+      val includeXml =
+        """
+          |<FindBugsFilter>
+          |    <Match>
+          |        <Bug category="SECURITY"/>
+          |    </Match>
+          |</FindBugsFilter>
+        """.stripMargin
+      IO.write(includeFile, includeXml)
+      includeFile
+    }
   }
 }
